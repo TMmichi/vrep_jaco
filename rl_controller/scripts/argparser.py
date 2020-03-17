28b18a4604709da791697c6c230c9b5153367adb
TMmichi <ljh_0921@yonsei.ac.kr>
2020-03-17 17:15:17 -0400

    max episode counts changed to 2000

--- rl_controller/scripts/argparser.py
+++ rl_controller/scripts/argparser.py
@@ -10,7 +10,7 @@ class ArgParser():
 
     def _add_arguments(self):
         self.parser.add_argument('--environ-string', default="jaco_table2", help='Environment id')
-        self.parser.add_argument('--max-episode-count', default=100, type=int, help='max number of training episodes') # 2000
+        self.parser.add_argument('--max-episode-count', default=2000, type=int, help='max number of training episodes') # 2000
         self.parser.add_argument('--seed', default=777, type=int, help='seed')
         self.parser.add_argument('__name', default="rl_controller", help='seed')
         self.parser.add_argument('__log', default="log_dir", help='seed')
